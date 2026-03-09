b02857456ba53b32bb9542f06452c9ee3aa25546
yrmisi <yrmisi@example.com>
2026-03-10 01:07:47 +0300

    add BookUpdate in schemas

--- books_library/schemas/books.py
+++ books_library/schemas/books.py
@@ -2,7 +2,7 @@ from datetime import date
 from typing import Annotated
 from uuid import UUID
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, ConfigDict, Field
 
 Title = Annotated[str, Field(min_length=1, max_length=500)]
 
@@ -29,4 +29,17 @@ class BookRead(BookBase):
 class BookUpdate(BookBase):
     """Update a book."""
 
-    title: Title
+    title: Title | None = None
+    pub_date: date | None = None
+
+    model_config = ConfigDict(
+        extra="ignore",
+        json_schema_extra={
+            "examples": [
+                {
+                    "title": "String",
+                    "pub_date": date(2026, 3, 9).isoformat(),
+                },
+            ]
+        },
+    )
