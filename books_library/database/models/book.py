90fd72b1745ca1dd6902faf76342ab655009149c
yrmisi <yrmisi@example.com>
2026-03-10 22:31:16 +0300

    add field author in model Book

--- books_library/database/models/book.py
+++ books_library/database/models/book.py
@@ -1,14 +1,24 @@
 from datetime import date
+from typing import TYPE_CHECKING
+from uuid import UUID
 
-from sqlalchemy import String, Text
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import ForeignKey, String, Text
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from .base import Base
+
+if TYPE_CHECKING:
+    from .author import Author
 
 
 class Book(Base):
     """Model Book."""
 
+    author_id: Mapped[UUID] = mapped_column(
+        ForeignKey("authors.id", ondelete="CASCADE"),
+        nullable=False,
+        index=True,
+    )
     title: Mapped[str] = mapped_column(
         Text,
         default="title",
@@ -16,8 +26,14 @@ class Book(Base):
         nullable=False,
         index=True,
     )
-    pub_date: Mapped[date]
     short_description: Mapped[str | None] = mapped_column(
         String(500),
         nullable=True,
+    )
+    pub_date: Mapped[date]
+
+    author: Mapped["Author"] = relationship(
+        "Author",
+        back_populates="books",
+        lazy="joined",
     )
