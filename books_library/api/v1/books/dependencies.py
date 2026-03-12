443084f6a191b46b587d1d8a362c93a450f69db1
yrmisi <yrmisi@example.com>
2026-03-12 23:46:43 +0300

    add function dependency

--- books_library/api/v1/books/dependencies.py
+++ books_library/api/v1/books/dependencies.py
@@ -1,78 +1,67 @@
-from datetime import date
 from typing import Annotated
 from uuid import UUID
 
 from fastapi import Depends
 
-from database.models import Author, Book
+from database.models import Book
 from dependencies.session import AsyncSessionDep
-from exceptions import AuthorNotFoundError, BookNotFoundError
 from schemas import BookCreate, BookUpdate
 
 from ..authors.dependencies import AuthorRepositoryDep
 from .repositories import BookRepository
+from .services import BookService
 
 
-def repository_provider(session: AsyncSessionDep) -> BookRepository:
+def repository_provider_dependency(session: AsyncSessionDep) -> BookRepository:
     return BookRepository(session)
 
 
-BookRepositoryDep = Annotated[BookRepository, Depends(repository_provider)]
+BookRepositoryDep = Annotated[BookRepository, Depends(repository_provider_dependency)]
 
 
-async def create_book(
-    book_create: BookCreate,
+def service_provider_dependency(
     book_repo: BookRepositoryDep,
     author_repo: AuthorRepositoryDep,
+) -> BookService:
+    return BookService(book_repo, author_repo)
+
+
+BookServiceDep = Annotated[BookService, Depends(service_provider_dependency)]
+
+
+async def create_book_dependency(
+    book_create: BookCreate,
+    book_service: BookServiceDep,
 ) -> Book:
-    author: Author | None = await author_repo.get_by_id(author_id=book_create.author_id)
-    if author is None:
-        raise AuthorNotFoundError(author_id=book_create.author_id)
-
-    return await book_repo.create(book_create=book_create)
+    return await book_service.create_book(book_create=book_create)
 
 
-BookCreateDep = Annotated[Book, Depends(create_book)]
+BookCreateDep = Annotated[Book, Depends(create_book_dependency)]
 
 
-async def get_books_list(book_repo: BookRepositoryDep) -> list[Book]:
-    return await book_repo.list()
+async def get_books_list_dependency(book_service: BookServiceDep) -> list[Book]:
+    return await book_service.get_books_list()
 
 
-BooksListDep = Annotated[list[Book], Depends(get_books_list)]
+BooksListDep = Annotated[list[Book], Depends(get_books_list_dependency)]
 
 
-async def get_book_by_id(book_id: UUID, book_repo: BookRepositoryDep) -> Book:
-    book = await book_repo.get_by_id(book_id=book_id)
-    if book is not None:
-        return book
-    raise BookNotFoundError(
-        book_id=book_id,
-    )
+async def get_book_by_id_dependency(
+    book_id: UUID,
+    book_service: BookServiceDep,
+) -> Book:
+    return await book_service.get_book_by_id(book_id=book_id)
 
 
-BookIDDep = Annotated[Book, Depends(get_book_by_id)]
+BookIDDep = Annotated[Book, Depends(get_book_by_id_dependency)]
 
 
-async def update_book(
+async def update_book_dependency(
     book_id: UUID,
     book_update: BookUpdate,
-    book_repo: BookRepositoryDep,
+    book_service: BookServiceDep,
 ) -> Book:
-    book: Book | None = await book_repo.get_by_id(book_id=book_id)
-
-    if book is None:
-        raise BookNotFoundError(book_id=book_id)
-
-    update_data: dict[str, str | date] = book_update.model_dump(exclude_unset=True)
-
-    for key, val in update_data.items():
-        setattr(book, key, val)
-
-    await book_repo.session.commit()
-    await book_repo.session.refresh(book)
-
-    return book
+    return await book_service.update_book(book_id=book_id, book_update=book_update)
 
 
-BookUpdateDep = Annotated[Book, Depends(update_book)]
+BookUpdateDep = Annotated[Book, Depends(update_book_dependency)]
